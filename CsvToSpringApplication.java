d499745ab945919b94df1209e4739c6534462087
Ruturajpal <98735716+Ruturajpal@users.noreply.github.com>
2022-10-06 21:09:26 +0530

    Add files via upload

---
+++ CsvToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToSpringApplication.class, args);
+	}
+
+}
